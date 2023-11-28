344795f18306af58b5648e020bb6e37ba54d505e
Iury <correia.matheusiury@gmail.com>
2023-11-27 21:45:50 -0300

    Using Threading and signals with PyQt5 and improving data acquisition. Still have stack overflow issues, but it's going in the right way.

--- t.py
+++ t.py
@@ -4,10 +4,9 @@ import threading
 import numpy as np
 import openpyxl
 import matplotlib.pyplot as plt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QComboBox
-from PyQt5.QtCore import Qt, QTimer, pyqtSignal, QObject, QMutex
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QComboBox
+from PyQt5.QtCore import Qt, QTimer, pyqtSignal, QObject, QMutex, QMutexLocker
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.animation import FuncAnimation
 
 
 class MainWindow(QMainWindow):
@@ -20,11 +19,11 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(main_widget)
 
         # Main layout (horizontal) to split the window
-        main_layout = QVBoxLayout(main_widget)
+        main_layout = QHBoxLayout(main_widget)
 
         # Create a widget with buttons on the left side
         button_widget = QWidget()
-        button_layout = QVBoxLayout(button_widget)
+        button_layout = QHBoxLayout(button_widget)
 
         # Add buttons
         self.start_button = QPushButton("Start Recording")
@@ -55,15 +54,16 @@ class MainWindow(QMainWindow):
         # Set up the main layout
         main_widget.setLayout(main_layout)
 
-    def toggle_recording(self):
+    def toggle_recording(self, data_updater=None):
         if self.start_button.text() == "Start Recording":
             self.start_button.setText("Stop Recording")
-            self.plot_widget.start_recording()
-
             # Get the selected max time from the combo box
             max_time_text = self.max_time_combobox.currentText()
             max_time_seconds = int(max_time_text.split()[0])  # Extract the numeric value
             self.plot_widget.set_max_time(max_time_seconds)
+
+            self.plot_widget.start_recording()
+
         else:
             self.start_button.setText("Start Recording")
             crucial_data = self.plot_widget.stop_recording()
@@ -100,7 +100,9 @@ class DataUpdater(QObject):
         self.serial_port = "COM5"  # Selecione a porta correta
         self.data_buffer = np.zeros(250)
         self.recording = False
+        print("StOv a")
         self.data_buffer_lock = QMutex()  # Adicione o lock para acesso seguro ao data_buffer
+        print("StOv b")
 
         try:
             self.arduino = serial.Serial(self.serial_port, 57600, timeout=1)
@@ -115,14 +117,26 @@ class DataUpdater(QObject):
         # Adicione um QTimer para atualizar a interface gráfica
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_data)
+        print("StOv c")
         self.timer.start(100)  # Atualizar a cada 100 ms
 
         # Thread para leitura dos dados
         self.thread = threading.Thread(target=self.read_serial_data)
         self.thread.start()
+        print("StOv d")
+
+        self.send_start_command()
+
+    def send_start_command(self):
+        try:
+            self.arduino.write(b'r')  # Envie a letra 'r' como bytes
+            print("Comando 'r' enviado para iniciar a leitura.")
+        except serial.SerialException as e:
+            print(f"Erro ao enviar o comando 'r' pela porta serial: {e}")
 
     def update_data(self):
-        with self.data_buffer_lock:
+        print("StOv e")
+        with QMutexLocker(self.data_buffer_lock):
             self.data_updated.emit(self.data_buffer)
 
     def read_serial_data(self):
@@ -140,7 +154,7 @@ class DataUpdater(QObject):
                     except ValueError:
                         print(f"Erro ao converter '{data_str}' para float")
 
-                    with self.data_buffer_lock:
+                    with QMutexLocker(self.data_buffer_lock):
                         self.data_buffer = np.roll(self.data_buffer, -1)
                         self.data_buffer[-1] = value
 
@@ -178,29 +192,34 @@ class LivePlotWidget(QWidget):
 
         # Adicione um QTimer para atualizar a interface gráfica
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_interface)
+        self.timer.timeout.connect(self.update_plot)
         self.timer.start(100)  # Atualizar a cada 100 ms
 
         self.ax.set_xlim(0, 100)  # Assuming 100 data points
         self.ax.set_ylim(0, 1024)
+        print("StOv f")
 
-    def update_interface(self):
-        with self.data_buffer_lock:
-            x_values = np.arange(len(self.data_buffer))
+    def update_plot(self, threshold):
+        print("StOv g")
+        with QMutexLocker(self.data_buffer_lock):
+            x_values = np.arange(len(self.data_buffer.copy()))
             y_values = self.data_buffer.copy()  # Crie uma cópia para garantir consistência
 
+        print("StOv h")
         self.plot_line.set_data(x_values, y_values)
         self.ax.relim()
         self.ax.autoscale_view()
         self.canvas.draw()
 
     def start_recording(self):
-        self.recording = True
+        self.threshold = 0.25    # Value of minimum strength
+
 
     def stop_recording(self):
         self.recording = False
+        print("StOv i")
         # Adicione o retorno dos dados cruciais
-        with self.data_buffer_lock:
+        with QMutexLocker(self.data_buffer_lock):
             return [np.max(self.data_buffer), np.mean(self.data_buffer), np.std(self.data_buffer)]
 
     def set_max_time(self, max_time_seconds):
@@ -213,8 +232,11 @@ def main():
 
         window = MainWindow()
         data_updater = DataUpdater()
-        data_updater.data_updated.connect(window.plot_widget.update_interface)
+        print("StOv 1")
+        data_updater.data_updated.connect(window.plot_widget.update_plot)
+        print("StOv 2")
         data_updater.start_thread()
+        print("StOv 3")
         window.show()
 
         sys.exit(app.exec_())
